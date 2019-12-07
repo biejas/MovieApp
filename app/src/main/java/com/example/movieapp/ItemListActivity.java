342b663d56def3280c733758a0e4ac944ccbe9d1
agnjasn <agnieszka.jasnosz07@gmail.com>
2019-12-07 16:08:26 +0100

    Add horizontal view to phone layout

--- app/src/main/java/com/example/movieapp/ItemListActivity.java
+++ app/src/main/java/com/example/movieapp/ItemListActivity.java
@@ -3,21 +3,19 @@ package com.example.movieapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.movieapp.dummy.DummyContent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
